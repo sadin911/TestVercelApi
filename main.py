37e355e3c3c6ad9581c58b6792e9265b242c1926
Chonlatid_PC2 <chonlatid.d@chanwanich.com>
2022-11-22 13:34:25 +0700

    update

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 from flask import Flask
 
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return ("<p>This is Flask from Vercel Serverless python</p>")
+
+@app.route('/')
+def home():
+    return 'Home Page Route'
+
+
+@app.route('/about')
+def about():
+    return 'About Page Route'
+
+
+@app.route('/portfolio')
+def portfolio():
+    return 'Portfolio Page Route'
+
+
+@app.route('/contact')
+def contact():
+    return 'Contact Page Route'
